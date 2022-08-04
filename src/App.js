d94b3f485033ec3a7547ab7021cff50159cad970
Buidlor <89695544+Buidlor@users.noreply.github.com>
2022-08-04 16:19:20 +0200

    all functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState({})
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(result=>result.json())
+      .then(data => {
+        setQuote({content: data.content, author: data.author})
+      })
+  } 
+
+  useEffect(()=>{
+    getQuote()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <p id="text">{quote.content}</p>
+        <p id="author">{quote.author}</p>
+        <button id="new-quote" onClick={getQuote}>New Quote</button><br/>
+        <a href={`https://twitter.com/intent/tweet?hashtags=quotes&hashtags=code&text=${quote.content}+${quote.author}` } target="_blank" rel="noreferrer" id="tweet-quote">post on twitter</a>
+      </div>
     </div>
   );
 }
